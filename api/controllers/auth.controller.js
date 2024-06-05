265dd3423e51396ff21da94ec22dccd4a801b52f
MCDoodle1 <mclarijs@me.com>
2024-06-05 12:14:53 +0200

    Add middleware and error function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import errorHandler from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -7,8 +8,8 @@ export const signup = async (req, res, next) => {
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).json("user created successfully");
+    res.status(201).json("User created successfully");
   } catch (error) {
-    res.status(500).json("user not created", error.message);
+    next(error);
   }
 };
