8b70bdedbe35853a0aebb214235c943050819ed0
MCDoodle1 <mclarijs@me.com>
2024-06-05 12:15:41 +0200

    Complete signup page UI and functionality

--- client/redux/user/userSlice.js
+++ client/redux/user/userSlice.js
@@ -22,8 +22,27 @@ const userSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     },
+    signUpStart: (state) => {
+      state.loading = true;
+    },
+    signUpSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    signUpFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
   },
 });
 
-export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  signUpStart,
+  signUpSuccess,
+  signUpFailure,
+} = userSlice.actions;
 export default userSlice.reducer;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,4 +1,85 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  signUpStart,
+  signUpSuccess,
+  signUpFailure,
+} from "../../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(signUpStart());
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        dispatch(signUpFailure(data.message));
+        return;
+      }
+      dispatch(signUpSuccess(data));
+      navigate("/");
+    } catch (error) {
+      dispatch(signUpFailure(error.message));
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  return (
+    <div className="signup__container">
+      <h1 className="signup__header">
+        Sign<span className="signup__header-light">Up</span>
+      </h1>
+      <form onSubmit={handleSubmit} className="signup__form">
+        <input
+          type="text"
+          placeholder="username"
+          className="signup__form-item"
+          id="username"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="signup__form-item"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="signup__form-item"
+          id="password"
+          onChange={handleChange}
+        />
+        <button disabled={loading} className="signup__button">
+          {loading ? "Loading..." : "Sign Up"}
+        </button>
+      </form>
+      <div className="signup__link">
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className="signup__link-text">Sign in</span>
+        </Link>
+      </div>
+      {error && <p className="">{error}</p>}
+    </div>
+  );
 };
 export default SignUp;
